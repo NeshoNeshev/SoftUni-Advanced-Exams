c1dfcc1f926ff1de33d5a6635c71aea3b967223a
Nesho Neshev <neshev1978@gmail.com>
2020-10-22 16:53:01 +0300

    Add files via upload

---
+++ C# Advanced Exam - 22 Feb 2020/1-LootBox/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lootbox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>(Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray());
+
+            Stack<int> stack = new Stack<int>(Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray());
+
+            //Queue<int> queue = new Queue<int>(queueNumbers);
+            //Stack<int> stack = new Stack<int>(stackNumbers);
+            
+
+            int value = 0;
+            bool isEpic = false;
+            while (true)
+            {
+                if (stack.Count == 0 || queue.Count == 0)
+                {
+                    break;
+                }
+                int sum = stack.Peek() + queue.Peek();
+                if (sum % 2 == 0)
+                {
+                    value += sum;
+                    stack.Pop();
+                    queue.Dequeue();
+                    
+                }
+                else
+                {
+                    queue.Enqueue(stack.Pop());
+                }
+            }
+            if (value >= 100)
+            {
+                isEpic = true;
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+                if (isEpic)
+                {
+                    Console.WriteLine($"Your loot was epic! Value: {value}");
+                }
+                else
+                {
+                    Console.WriteLine($"Your loot was poor... Value: {value}");
+                }
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+                if (isEpic)
+                {
+                    Console.WriteLine($"Your loot was epic! Value: {value}");
+                }
+                else
+                {
+                    Console.WriteLine($"Your loot was poor... Value: {value}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced Exam - 22 Feb 2020/3-Guild/Guild.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private List<Player> Players;
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Players = new List<Player>();
+
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        //public List<Player> Players;
+        public Player[] KickedPlayrs { get; set; }
+        public int Count { get { return this.Players.Count(); } }
+        public void AddPlayer(Player player)
+        {
+            
+            if (Capacity > 0)
+            {
+                Players.Add(player);
+            }
+            this.Capacity--;
+        }
+        public bool RemovePlayer(string name)
+        {
+            var exist = Players.Where(x => x.Name == name).FirstOrDefault();
+            if (exist != null)
+            {
+                Players.Remove(exist);
+                return true;
+            }
+            return false;
+        }
+        public void PromotePlayer(string name)
+        {
+            var exist = Players.Where(x => x.Name == name).Where(r => r.Rank != "Member").FirstOrDefault();
+            if (exist != null)
+            {
+                exist.Rank = "Member";
+            }
+        }
+        public void DemotePlayer(string name)
+        {
+            var exist = Players.Where(x => x.Name == name).Where(r => r.Rank == "Member").FirstOrDefault();
+            if (exist != null)
+            {
+                exist.Rank = "Trial";
+            }
+        }
+        public Player[] KickPlayersByClass(string classs)
+        {
+
+            Player[] kickedPlayers = Players.Where(c => c.Class == classs).ToArray();
+            foreach (var item in kickedPlayers)
+            {
+                Players.Remove(item);
+            }
+            return kickedPlayers;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {Name}");
+            foreach (var item in Players)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            
+
+            return sb.ToString().Trim();
+
+        }
+
+    }
+}
