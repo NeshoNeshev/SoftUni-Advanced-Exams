f09d58b157476c6e65cb3348fc31ccc158165cd7
Nesho Neshev <neshev1978@gmail.com>
2020-10-22 16:54:04 +0300

    Add files via upload

---
+++ C# Advanced Exam - 24 Feb 2019/3-Heroes/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Heroes
+{
+    
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            HeroRepository repository = new HeroRepository();
+            //Initialize entity
+            Item item = new Item(23, 35, 48);
+            //Print Item
+            Console.WriteLine(item);
+            Hero hero = new Hero("Hero Name", 24, item);
+            //Print Hero
+            Console.WriteLine(hero);
+
+            repository.Add(hero);
+            //Remove Hero
+            repository.Remove("Hero Name");
+
+            Item secondItem = new Item(100, 20, 13);
+            Hero secondHero = new Hero("Second Hero Name", 125, secondItem);
+
+            //Add Heroes
+            repository.Add(hero);
+            repository.Add(secondHero);
+
+            Hero heroStrength = repository.GetHeroWithHighestStrength(); // Hero with name Second Hero
+            Hero heroAbility = repository.GetHeroWithHighestAbility(); // Hero with name Hero Name
+            Hero heroIntelligence = repository.GetHeroWithHighestIntelligence(); // Hero with name Hero
+
+            Console.WriteLine(repository.Count); //2
+
+            Console.WriteLine(repository);
+
+
+
+        }
+    }
+}
