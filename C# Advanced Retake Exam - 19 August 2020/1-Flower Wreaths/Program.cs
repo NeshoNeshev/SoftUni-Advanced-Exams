3dbfec7811b7fd4e2a1914e1fe7a9d25bf125809
Nesho Neshev <neshev1978@gmail.com>
2020-10-22 16:57:30 +0300

    Add files via upload

---
+++ C# Advanced Retake Exam - 19 August 2020/1-Flower Wreaths/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lilies = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var roses = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(lilies);
+            Queue<int> queue = new Queue<int>(roses);
+            int flowers = 0;
+            int counter = 0;
+            while (true)
+            {
+                if (stack.Count == 0 || queue.Count == 0)
+                {
+                    break;
+                }
+                int lilie = stack.Pop();
+                if (lilie + queue.Peek() == 15)
+                {
+                    counter++;
+                    queue.Dequeue();
+                }
+                else if (lilie + queue.Peek() < 15)
+                {
+                    flowers += queue.Dequeue() + lilie;
+                }
+                else if (lilie + queue.Peek() > 15)
+                {
+
+                    lilie -= 2;
+                    stack.Push(lilie);
+                }
+            }
+            if (flowers > 15)
+            {
+                counter += flowers / 15; ;
+            }
+
+            if (counter >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {counter} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - counter} wreaths more!");
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced Retake Exam - 19 August 2020/2-Bee/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+
+namespace _02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            int startingRow = -1;
+            int startingCol = -1;
+            int counter = 0;
+            bool isWin = false;
+            FilMatrix(matrix, ref startingRow, ref startingCol);
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command=="End")
+                {
+                    isWin = true;
+                    break;
+                }
+                if (command=="up")
+                {
+                    matrix[startingRow, startingCol] = '.';
+                    startingRow--;
+                    if (!IsInside(matrix,startingRow,startingCol))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (Bonus(matrix,startingRow,startingCol))
+                        {
+                            matrix[startingRow, startingCol] = '.';
+                            startingRow--;
+                            if (!IsInside(matrix,startingRow,startingCol))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                if (Counter(matrix,startingRow,startingCol))
+                                {
+                                    counter++;
+                                }
+                                matrix[startingRow, startingCol] = 'B';
+                            }
+                           
+                        }
+                        if (Counter(matrix, startingRow, startingCol))
+                        {
+                            counter++;
+                        }
+                        matrix[startingRow, startingCol] = 'B';
+                    }
+                }
+                if (command == "down")
+                {
+                    matrix[startingRow, startingCol] = '.';
+                    startingRow++;
+                    if (!IsInside(matrix, startingRow, startingCol))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (Bonus(matrix, startingRow, startingCol))
+                        {
+                            matrix[startingRow, startingCol] = '.';
+                            startingRow++;
+                            if (!IsInside(matrix, startingRow, startingCol))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                if (Counter(matrix, startingRow, startingCol))
+                                {
+                                    counter++;
+                                }
+                                matrix[startingRow, startingCol] = 'B';
+                            }
+
+                        }
+                        if (Counter(matrix, startingRow, startingCol))
+                        {
+                            counter++;
+                        }
+                        matrix[startingRow, startingCol] = 'B';
+                    }
+                }
+                if (command == "left")
+                {
+                    matrix[startingRow, startingCol] = '.';
+                    startingCol--;
+                    if (!IsInside(matrix, startingRow, startingCol))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (Bonus(matrix, startingRow, startingCol))
+                        {
+                            matrix[startingRow, startingCol] = '.';
+                            startingCol--;
+                            if (!IsInside(matrix, startingRow, startingCol))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                if (Counter(matrix, startingRow, startingCol))
+                                {
+                                    counter++;
+                                }
+                                matrix[startingRow, startingCol] = 'B';
+                            }
+
+                        }
+                        if (Counter(matrix, startingRow, startingCol))
+                        {
+                            counter++;
+                        }
+                        matrix[startingRow, startingCol] = 'B';
+                    }
+                }
+                if (command == "right")
+                {
+                    matrix[startingRow, startingCol] = '.';
+                    startingCol++;
+                    if (!IsInside(matrix, startingRow, startingCol))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (Bonus(matrix, startingRow, startingCol))
+                        {
+                            matrix[startingRow, startingCol] = '.';
+                            startingCol++;
+                            if (!IsInside(matrix, startingRow, startingCol))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                if (Counter(matrix, startingRow, startingCol))
+                                {
+                                    counter++;
+                                }
+                                matrix[startingRow, startingCol] = 'B';
+                            }
+
+                        }
+                        if (Counter(matrix, startingRow, startingCol))
+                        {
+                            counter++;
+                        }
+                        matrix[startingRow, startingCol] = 'B';
+                    }
+                }
+                
+            }
+            if (!isWin)
+            {
+                Console.WriteLine("The bee got lost!");
+                if (counter>=5)
+                {
+                    Console.WriteLine($"Great job, the bee managed to pollinate {counter} flowers!");
+                }
+                else
+                {
+                    Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5-counter} flowers more");
+                }
+            }
+            else
+            {
+                if (counter >= 5)
+                {
+                    Console.WriteLine($"Great job, the bee managed to pollinate {counter} flowers!");
+                }
+                else
+                {
+                    Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - counter} flowers more");
+                }
+            }
+            PrintMatrix(matrix);
+        }
+        public static bool Counter(char[,] matrix, int row, int col)
+        {
+            
+            if (matrix[row,col]=='f')
+            {
+                return true;
+            }
+            return false;
+        
+        }
+        public static bool Bonus(char[,] matrix, int row, int col)
+        {
+            if (matrix[row,col]=='O')
+            {
+                return true;
+            }
+            return false;
+        
+        }
+        private static void FilMatrix(char[,] matrix, ref int startingRow, ref int startingCol)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] chars = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = chars[col];
+                    if (matrix[row, col] == 'B')
+                    {
+                        startingRow = row;
+                        startingCol = col;
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+       
+        private static bool IsInside(char[,] matrix, int targetRow, int targetCol)
+        {
+            return targetRow >= 0 && targetRow < matrix.GetLength(0)
+                   && targetCol >= 0 && targetCol < matrix.GetLength(1);
+
+        }
+    }
+}
