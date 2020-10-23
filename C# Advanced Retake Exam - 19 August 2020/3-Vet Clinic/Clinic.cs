7daab961d2bf556036cb30a4e227cb244bb0ed61
Nesho Neshev <neshev1978@gmail.com>
2020-10-23 10:44:24 +0300

    Add files via upload

---
+++ C# Advanced Retake Exam - 19 August 2020/3-Vet Clinic/Clinic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    class Clinic
+    {
+        private List<Pet> pets;
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.pets = new List<Pet>();
+        }
+        public int Capacity { get; set; }
+        public int Count { get { return pets.Count(); } }
+        public void Add(Pet pet)
+        {
+            if (this.Capacity > 0)
+            {
+                pets.Add(pet);
+                this.Capacity--;
+            }
+        }
+        public bool Remove(string name)
+        {
+            var pet = pets.Where(p => p.Name == name).FirstOrDefault();
+            if (pet !=null)
+            {
+                pets.Remove(pet);
+                Capacity++;
+                return true;
+            }
+            return false;
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            var petAndOwner = pets.Where(p => p.Name == name).Where(o => o.Owner == owner).FirstOrDefault();
+            if (petAndOwner==null)
+            {
+
+                
+                return null;
+            }
+            else
+            {
+                return petAndOwner;
+            }
+        }
+        public Pet GetOldestPet()
+        {
+            if (pets.Count>0)
+            {
+                var oldestPet = pets.OrderByDescending(p => p.Age).ToArray();
+                return oldestPet[0];
+            }
+            return null;
+            
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+
+
+        }
+    }
+}
---
+++ C# Advanced Retake Exam - 19 August 2020/3-Vet Clinic/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    class Pet
+    {
+        public Pet(string name,int age,string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
