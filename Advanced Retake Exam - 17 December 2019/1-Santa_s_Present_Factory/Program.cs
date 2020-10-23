f2fcbd5731727f8d89a91b5dd0e2c8ef870564cc
Nesho Neshev <neshev1978@gmail.com>
2020-10-23 16:44:50 +0300

    Add files via upload

---
+++ Advanced Retake Exam - 17 December 2019/1-Santa_s_Present_Factory/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Santa_s_Present_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int doll = 150;
+            const int woodenTrain = 250;
+            const int tedyBear = 300;
+            const int bicycle = 400;
+            string bicycleCount = "Bicycle";
+            string dollCount = "Doll";
+            string bearCount = "Teddy bear";
+            string trainCount = "Wooden train";
+
+            Dictionary<string, int> gifts = new Dictionary<string, int>();
+
+            gifts.Add(bicycleCount, 0);
+            gifts.Add(dollCount, 0);
+            gifts.Add(bearCount, 0);
+            gifts.Add(trainCount, 0);
+
+
+            var stack = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var queue = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> materials = new Stack<int>(stack);
+            Queue<int> magics = new Queue<int>(queue);
+
+            while (true)
+            {
+                if (materials.Count == 0 || magics.Count == 0)
+                {
+                    break;
+                }
+                if (magics.Peek() == 0|| materials.Peek() == 0)
+                {
+                    if (magics.Peek() == 0)
+                    {
+                        magics.Dequeue();
+                        
+                    }
+                    if (materials.Peek() == 0)
+                    {
+                        materials.Pop();
+                       
+                    }
+                    continue;
+                }
+                
+                int result = magics.Peek() * materials.Peek();
+                if (result < 0)
+                {
+                    int newMaterials = materials.Pop() + magics.Dequeue();//
+                    if (newMaterials>0)
+                    {
+                        materials.Push(newMaterials);
+                    }
+                    
+                }
+                else if (result == doll)
+                {
+                    gifts[dollCount]++;
+                    magics.Dequeue();
+                    materials.Pop();
+                }
+                else if (result == woodenTrain)
+                {
+                    gifts[trainCount]++;
+                    magics.Dequeue();
+                    materials.Pop();
+                }
+                else if (result == tedyBear)
+                {
+                    gifts[bearCount]++;
+                    magics.Dequeue();
+                    materials.Pop();
+                }
+                else if (result == bicycle)
+                {
+                    gifts[bicycleCount]++;
+                    magics.Dequeue();
+                    materials.Pop();
+                }
+                else
+                {
+                    magics.Dequeue();
+                    materials.Push(materials.Pop() + 15);
+                }
+
+
+            }
+            var sorted = gifts.OrderBy(x => x.Key).Where(x => x.Value > 0);
+            if ((gifts[dollCount] > 0 && gifts[trainCount] > 0) || (gifts[bearCount] > 0 && gifts[bicycleCount] > 0))
+            {
+                Console.WriteLine("The presents are crafted! Merry Christmas!");
+                if (materials.Count > 0)
+                {
+                    Console.WriteLine($"Materials left: {string.Join(", ", materials)}");
+                }
+                if (magics.Count > 0)
+                {
+                    Console.WriteLine($"Magic left: {string.Join(", ", magics)}");
+                }
+                foreach (var item in sorted)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No presents this Christmas!");
+                if (materials.Count > 0)
+                {
+                    Console.WriteLine($"Materials left: {string.Join(", ", materials)}");
+                }
+                if (magics.Count > 0)
+                {
+                    Console.WriteLine($"Magic left: {string.Join(", ", magics)}");
+                }
+                foreach (var item in sorted)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+
+    }
+}
---
+++ Advanced Retake Exam - 17 December 2019/Present Delivery/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Linq;
+
+namespace Present_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPresents = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int startingRow = -1;
+            int startingCol = -1;
+            int niceKid = 0;
+
+            FillMatrix(matrix, ref startingRow, ref startingCol, ref niceKid);
+            int count = niceKid;
+            matrix[startingRow, startingCol] = '-';
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Christmas morning" || numberPresents <= 0)
+                {
+                    break;
+                }
+                
+                switch (command)
+                {
+                    case "up":
+                        startingRow--;
+                        break;
+                    case "down":
+                        startingRow++;
+                        break;
+                    case "left":
+                        startingCol--;
+                        break;
+                    case "right":
+                        startingCol++;
+                        break;
+                }
+                if (IsInside(matrix, startingRow, startingCol))
+                {
+                    if (matrix[startingRow, startingCol] == 'V')
+                    {
+                        niceKid--;
+                        numberPresents--;
+                        matrix[startingRow, startingCol] = '-';
+                    }
+                    if (matrix[startingRow, startingCol] == 'X')
+                    {
+                        matrix[startingRow, startingCol] = '-';
+                    }
+                    if (IsHappy(matrix, startingRow, startingCol))
+                    {
+                        if (matrix[startingRow - 1, startingCol] != '-')
+                        {
+                            if (NiceKid(matrix, startingRow - 1, startingCol))
+                            {
+                                niceKid--;
+                            }
+                            numberPresents--;
+                            matrix[startingRow - 1, startingCol] = '-';
+                            if (numberPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                        if (matrix[startingRow + 1, startingCol] != '-')
+                        {
+                            if (NiceKid(matrix, startingRow + 1, startingCol))
+                            {
+                                niceKid--;
+                            }
+                            numberPresents--;
+                            matrix[startingRow + 1, startingCol] = '-';
+                            if (numberPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                        if (matrix[startingRow, startingCol - 1] != '-')
+                        {
+                            if (NiceKid(matrix, startingRow, startingCol - 1))
+                            {
+                                niceKid--;
+                            }
+                            numberPresents--;
+                            matrix[startingRow, startingCol - 1] = '-';
+                            if (numberPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                        if (matrix[startingRow, startingCol + 1] != '-')
+                        {
+                            if (NiceKid(matrix, startingRow, startingCol + 1))
+                            {
+                                niceKid--;
+                            }
+                            numberPresents--;
+                            matrix[startingRow, startingCol + 1] = '-';
+                            if (numberPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            matrix[startingRow, startingCol] = 'S';
+            if (numberPresents == 0)
+            {
+                Console.WriteLine("Santa ran out of presents!");
+            }
+
+            PrintMatrix(matrix);
+            if (niceKid == 0)
+            {
+                Console.WriteLine($"Good job, Santa! {count} happy nice kid/s.");
+            }
+            else
+            {
+                Console.WriteLine($"No presents for {niceKid} nice kid/s.");
+            }
+        }
+        public static bool NiceKid(char[,] matrix, int row, int col)
+        {
+            if (matrix[row, col] == 'V')
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsHappy(char[,] matrix, int row, int col)
+        {
+            if (matrix[row, col] == 'C')
+            {
+                return true;
+            }
+            return false;
+        }
+        private static void FillMatrix(char[,] matrix, ref int startingRow, ref int startingCol, ref int niceKid)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var chars = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = chars[col];
+                    if (matrix[row, col] == 'S')
+                    {
+                        startingRow = row;
+                        startingCol = col;
+                    }
+                    if (matrix[row, col] == 'V')
+                    {
+                        niceKid++;
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] cheesBoard)
+        {
+            for (int row = 0; row < cheesBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < cheesBoard.GetLength(1); col++)
+                {
+                    Console.Write(cheesBoard[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        private static bool IsInside(char[,] board, int targetRow, int targetCol)
+        {
+            return targetRow >= 0 && targetRow < board.GetLength(0)
+                   && targetCol >= 0 && targetCol < board.GetLength(1);
+
+        }
+    }
+}
