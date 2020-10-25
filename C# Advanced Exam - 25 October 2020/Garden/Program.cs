f7e25aa6ae8e1e5326436e0beef81a6c3a37b420
Nesho Neshev <neshev1978@gmail.com>
2020-10-25 17:21:03 +0200

    Add files via upload

---
+++ C# Advanced Exam - 25 October 2020/Garden/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int r = nums[0];
+            int c = nums[1];
+            int[,] matrix = new int[r, c];
+            FillMatrix(matrix);
+            
+            var command = Console.ReadLine().Split();
+            while (true)
+            {
+                if (command.Contains("Plow"))
+                {
+                    break;
+                }
+                int row = int.Parse(command[0]);
+                int col = int.Parse(command[1]);
+                if (!IsInside(matrix, row, col))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+                else
+                {
+                    for (int i = 0; i < matrix.GetLength(1); i++)
+                    {
+                        matrix[row, i] += 1;
+                    }
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                    {
+                        matrix[j, col] += 1;
+                    }
+                    matrix[row, col] -= 1;
+                }
+                command = Console.ReadLine().Split();
+            }
+            PrintMatrix(matrix);
+        }
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] cheesBoard)
+        {
+            for (int row = 0; row < cheesBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < cheesBoard.GetLength(1); col++)
+                {
+                    Console.Write(cheesBoard[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static bool IsInside(int[,] board, int targetRow, int targetCol)
+        {
+            return targetRow >= 0 && targetRow < board.GetLength(0)
+                   && targetCol >= 0 && targetCol < board.GetLength(1);
+
+        }
+    }
+}
---
+++ C# Advanced Exam - 25 October 2020/Scheduling/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var thread = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+           
+            int num = int.Parse(Console.ReadLine());
+            
+            Queue<int> threads = new Queue<int>(thread);
+            Stack<int> tasks = new Stack<int>(task);
+            int tr = 0;
+            while (true)
+            {
+                int a = tasks.Peek();
+                int b = threads.Peek();
+                if (threads.Peek() >= tasks.Peek())
+                {
+                    
+                    if (tasks.Peek()==num)
+                    {
+                        tr = threads.Peek();
+                        
+                        break;
+                    }
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else
+                {
+                    if (tasks.Peek() == num)
+                    {
+                        tr = threads.Peek();
+                        
+                        break;
+                    }
+                    threads.Dequeue();
+                }
+            }
+            Console.WriteLine($"Thread with value {tr} killed task {num}");
+            Console.WriteLine(string.Join(" ",threads));
+            
+        }
+    }
+}
